c2b2cdce8bd856ccb6a2183224a0658cc400de72
hamzamk <hamza@phantomore.com>
2018-09-07 18:24:20 +0800

    Create replace.py

---
+++ replace.py
@@ -0,0 +1,38 @@
+import os
+
+folder_name= 'test'
+path = '{}/{}'.format(os.getcwd(), folder_name )
+extension = 'xml'
+expression, replacement = 'testing_images', 'test'
+
+
+def main():
+	matched = []
+
+	def replace_all(file, search_exp, replace_exp):
+		with open(file, "r", encoding="ISO-8859-1") as f:
+			xml_str = f.read()
+			if search_exp not in xml_str:
+				# next line is for debugging
+				#print('"{}" not found in {}.'.format(search_exp, file))
+				return
+			else:
+				with open(file, "w", encoding="ISO-8859-1") as s:
+					matched.append(file)
+					# next line is for debugging
+					# print('Changing "{}" to "{}" in {}'.format(search_exp, replace_exp, f))
+					print('found')
+					xml_str = xml_str.replace(search_exp, replace_exp)
+					s.write(xml_str)
+
+	contents = []
+	for filename in os.listdir(path):
+		if filename.endswith(extension):
+			contents.append(filename)
+			replace_all('{}/{}'.format(path, filename), expression, replacement)
+
+	print('The directory is {} and a total of {} files found with extension {} with {} matches'.format(path, len(contents), extension, len(matched)))
+
+
+if __name__ == '__main__':
+	main()
